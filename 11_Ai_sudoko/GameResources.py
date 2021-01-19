176fe1b00b33619a273a46c77611239bd3a4f0a3
kartikey shaurya <kartikeyshaurya@gmail.com>
2021-01-19 12:10:26 +0530

    udacity file

--- 11_Ai_sudoko/GameResources.py
+++ 11_Ai_sudoko/GameResources.py
@@ -1,15 +1,14 @@
 import pygame
 
 def load_image(name):
-    '''
-    Loading the image 
-    '''
+    """A better load of images."""
+    fullname = os.path.join("images", name)
     try:
-        image = pygame.image.load(name)
+        image = pygame.image.load(fullname)
         if image.get_alpha() == None:
             image = image.convert()
         else:
             image = image.convert_alpha()
-    except pygame.error :
-        print("Could not load tje image")
+    except pygame.error:
+        print("Oops! Could not load image:", fullname)
     return image, image.get_rect()
