11bdc3bd146dcdc0f0207bb825c4c9877424aa9f
kartikey shaurya <kartikeyshaurya@gmail.com>
2021-02-02 06:20:31 +0530

    trying hindi streamer

--- a.py
+++ a.py
@@ -1,8 +1,20 @@
-for i in {10..50} 
-do 
- 
-	link="https://edx-video.net/University_of_TorontoXUTQML101x-V00" 
-	rest="00_DTH.mp4" 
-	wget "$link$i$rest" 
- 
-done 
+import nltk 
+from nltk.stem.snowball import SnowballStemmer 
+  
+#the stemmer requires a language parameter 
+snow_stemmer = SnowballStemmer(language='arabic') 
+  
+#list of tokenized words 
+words = ["ख़रीदारों के लिए मार्ग दर्शिका"] 
+
+import pandas as pd 
+
+#stem's of each word 
+stem_words = [] 
+for w in words: 
+    x = snow_stemmer.stem(w) 
+    stem_words.append(x) 
+      
+#print stemming results 
+for e1,e2 in zip(words,stem_words): 
+    print(e1+' ----> '+e2)
